d36728a641e7a16c573a32dc69ecf3cfec819f30
replicantfrommars2501 <wviiwvii@icloud.com>
2025-07-22 13:51:36 +0300

    feat: add reverse function to invert digits of a number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const numberAsString = Math.abs(n).toString();
+  const arrayOfDigits = numberAsString.split('');
+  const reversedArrayOfDigits = arrayOfDigits.reverse();
+  const reversedNumberAsString = reversedArrayOfDigits.join('');
+  const digitsReversedNumber = parseInt(reversedNumberAsString, 10);
+
+  return digitsReversedNumber;
 };
